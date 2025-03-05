b1729ee759b4f3ab044d39da9fe073424ddf1464
Tenzin1799 <131330402+Tenzin1799@users.noreply.github.com>
2025-03-04 22:51:55 -0600

    First 'commit' with code.

---
+++ SpringBoot/src/test/java/com/AI_PoweredFraudDetectionSystemSpringBoot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AI_PoweredFraudDetectionSystemSpringBoot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
